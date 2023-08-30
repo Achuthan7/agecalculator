7591b5503fb21077dfa7e3fb5b6e4202f6d30705
Achuthan7 <141254121+Achuthan7@users.noreply.github.com>
2023-08-30 10:49:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ function calculateAge()
     const cm=today.getMonth();
     const cy=today.getFullYear();
     var age=cy-by;
-    alert("jd);
-    if(bm==cm)
+    alert(cd);
+    if(bm==cm+1)
     {
         if(bd<cd)
              document.getElementById('res').innerText=`Age:${age+1}`;
